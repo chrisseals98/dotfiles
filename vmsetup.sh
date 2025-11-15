067ccc23008adac286d784ff4698396153e1fd8f
Chris <chrisseals9893@gmail.com>
2025-11-15 11:12:37 -0500

    Add files via upload

---
+++ vmsetup.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/bash
+countdown() {
+    local count=$1
+    while [ $count -gt 0 ]; do
+        echo -ne "$count.."
+        sleep 1
+        ((count--))
+    done
+}
+
+echo "Starting zsh script..."
+countdown 3
+
+sudo pacman -S --noconfirm virtualbox-guest-utils
+
+echo "Starting vm service..."
+
+sudo systemctl enable vboxservice
+sudo systemctl start vboxservice
+
+echo "...Done!"
+
+return 0
